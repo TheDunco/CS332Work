568872e407960b1f2479f3fd679e33a3968bf3e8
TheDunco <duncvan@gmail.com>
2021-10-12 18:06:54 -0400

    Actually hosting works, putting default back to localhost

--- whiteboard/server.py
+++ whiteboard/server.py
@@ -108,10 +108,10 @@ def getNetworkIp():
 
 # Run websocket server on port PORT on the local loopback interface while you are
 # still debugging your own code.
-# start_server = websockets.serve(per_client_handler, "localhost", PORT)
+start_server = websockets.serve(per_client_handler, "localhost", PORT)
 # TODO: use this next line when you are ready to deploy and test your code with others.
 # (And comment out the line above.)
-start_server = websockets.serve(per_client_handler, getNetworkIp(), PORT)
+# start_server = websockets.serve(per_client_handler, getNetworkIp(), PORT)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
